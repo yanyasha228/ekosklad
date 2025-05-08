901a807f8cf172267ebecbee54a4717d1fac9e44
Your Name <ynddw@ynddw>
2025-05-08 23:51:48 +0300

    Fix EMAIL SERVICE BUG

--- src/main/java/com/ekoskladvalidator/ExProductCheck/ExProductCheckService.java
+++ src/main/java/com/ekoskladvalidator/ExProductCheck/ExProductCheckService.java
@@ -8,8 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClientException;
@@ -33,16 +32,37 @@ public class ExProductCheckService {
     private static final Logger log = LoggerFactory.getLogger(ExProductCheckService.class);
 
     public List<ProductCheck> checkProducts() {
+
         List<Product> products = productService.findAll()
                 .stream()
                 .filter(product -> Objects.nonNull(product.getUrlForValidating())).collect(Collectors.toList());
+
         int size = products.size();
         List<ProductCheck> productChecks = new CopyOnWriteArrayList<>();
         AtomicInteger counter = new AtomicInteger(0);
-        products.stream().parallel().forEach(product -> {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                + "AppleWebKit/537.36 (KHTML, like Gecko) "
+                + "Chrome/113.0.0.0 Safari/537.36");
+        headers.set("Accept-Language", "ru-RU,ru;q=0.9,en;q=0.8");
+        headers.set("Accept", "text/html");
+
+//        ResponseEntity<String> forEntity = restTemplate.getForEntity("https://kratki.ua/ru/mba-17-gilotina-ru", String.class);
+
+
+        products.forEach(product -> {
             try {
+
                 log.error(product.getUrlForValidating());
-                restTemplate.getForEntity(product.getUrlForValidating(), String.class);
+//                restTemplate.getForEntity(product.getUrlForValidating(), String.class);
+                HttpEntity<String> entity = new HttpEntity<>(headers);
+                ResponseEntity<String> response = restTemplate.exchange(
+                        product.getUrlForValidating(),
+                        HttpMethod.GET,
+                        entity,
+                        String.class
+                );
                 log.error("_____________________________________________VALID");
             } catch (Exception e) {
                 productChecks.add(new ProductCheck(product, HttpStatus.NOT_FOUND, e.getMessage()));
